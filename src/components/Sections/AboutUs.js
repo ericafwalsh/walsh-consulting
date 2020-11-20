58ed858274a7e6d4872414fc67e127f1f9de5926
Erica Walsh <erica.f.walsh@gmail.com>
2020-11-19 20:27:51 -0500

    Create AboutUs.js

---
+++ src/components/Sections/AboutUs.js
@@ -0,0 +1,14 @@
+// import styled from "styled-components"
+// import { Link } from "gatsby"
+// import PropTypes from "prop-types"
+import React from "react"
+
+// main green: 	#04733c
+// black: #040404
+// light green: #045c40
+// dark green: #042420
+// navy: #040424
+
+const AboutUs = () => <h2>Meet the Team</h2>
+
+export default AboutUs
