c9bdb6be49e7e648240ba87eb6da3e359c6a8852
Erica Walsh <ewalsh@yieldstreet.com>
2020-11-19 20:28:12 -0500

    Add files via upload

---
+++ src/components/Sections/Testimonials.js
@@ -0,0 +1,33 @@
+// import styled from "styled-components"
+// import { Link } from "gatsby"
+// import PropTypes from "prop-types"
+import React from "react"
+
+// main green: 	#04733c
+// black: #040404
+// light green: #045c40
+// dark green: #042420
+// navy: #040424
+
+const Testimonials = () => (
+  <div>
+    <h2>Testimonials</h2>
+    <h5>Jann Wenner</h5>
+    <p>Founder, Chief Executive Officer, Wenner Media</p>
+    <i>
+      “Tim is a skillful, financial leader. His business acumen, integrity,
+      honesty, and tireless dedication has proven invaluable to Wenner Media and
+      my family throughout our nearly 30 year relationship.”
+    </i>
+    <h5>Gus Wenner</h5>
+    <p>President, Chief Operating Officer, Wenner Media</p>
+    <i>
+      “Tim was instrumental in the operation and sale of Wenner Media and is
+      steadfast in his commitment to my family. I am extremely pleased that he
+      has established Walsh Consulting and that he will continue to manage my
+      Family’s wealth.”
+    </i>
+  </div>
+)
+
+export default Testimonials
---
+++ src/components/Sections/WhatWeDo.js
@@ -0,0 +1,44 @@
+// import styled from "styled-components"
+// import { Link } from "gatsby"
+// import PropTypes from "prop-types"
+import React from "react"
+
+// main green: 	#04733c
+// black: #040404
+// light green: #045c40
+// dark green: #042420
+// navy: #040424
+
+const WhatWeDo = () => (
+  <div>
+    <h2>What We Do</h2>
+    <h4>Family Financial Management</h4>
+    <p>
+      For nearly three decades, Walsh Consulting founder, Timothy Walsh, has
+      served as Chief Financial Officer for Wenner Media while managing the
+      Wenner family office. Mr. Walsh is a trustee of the family trusts and was
+      a board member of the operating company. His wide- ranging experience
+      provides a unique perspective on the challenges and opportunities facing
+      wealthy families. Having recognized an essential need in the marketplace
+      for customized family financial management, Mr. Walsh developed Walsh
+      Consulting. An expert support team has been assembled to provide a full
+      suite of services.
+    </p>
+    <p>
+      Tailored to meet the diverse needs of each family client, Walsh Consulting
+      maintains close contact with attorneys, investment advisors, accoun-
+      tants, tax professionals, insur- ance agents, estate managers, architects,
+      aviation professionals, appraisal firms and more. Personalized service,
+      customized plans and a dedicated team guarantee satisfaction and peace of
+      mind.
+    </p>
+    <p>
+      Successful financial relationships require trust, confidence, loyalty and
+      confidentiality, exactly what you can expect from Walsh Consulting.
+      Advanced technology and superior security measures protect each client’s
+      information.
+    </p>
+  </div>
+)
+
+export default WhatWeDo
