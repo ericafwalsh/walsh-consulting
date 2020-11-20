f8586b0c7317bfc471b5d26da9a6983a1cde9f85
Erica <ewalsh@yieldstreet.com>
2020-11-19 20:40:57 -0500

    inital commit

--- src/components/Sections/Contact.js
+++ src/components/Sections/Contact.js
@@ -1,7 +1,7 @@
 // import styled from "styled-components"
 // import { Link } from "gatsby"
 // import PropTypes from "prop-types"
-import React from "react"
+import React from "react";
 
 // main green: 	#04733c
 // black: #040404
@@ -9,6 +9,12 @@ import React from "react"
 // dark green: #042420
 // navy: #040424
 
-const Contact = () => <h2>Contact Us</h2>
+const Contact = () => (
+  <div>
+    <h2>Contact Us</h2>
+    <p>Timwalsh@walshconsultingLLC.com</p>
+    <p>917-304-2597</p>
+  </div>
+);
 
-export default Contact
+export default Contact;
